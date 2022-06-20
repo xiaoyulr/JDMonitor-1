aac4734a42372d47c635f0fee99070290af1e5e5
tcz0632 <tcz0632@163.com>
2022-06-20 13:31:33 +0800

    修改购物车锦鲤助力方式

--- t_cart_koi.js
+++ t_cart_koi.js
@@ -84,7 +84,6 @@ if ($.isNode()) {
             if ($.index % 3 == 0) await $.wait(parseInt(Math.random() * 5000 + 20000, 10))
         }
     }
-    console.log(`重新跑前10个号`)
     for (let i = 0; i < cookiesArr.length; i++) {
         if (cookiesArr[i]) {
             cookie = cookiesArr[i];
@@ -464,15 +463,15 @@ async function dealReturn(type, data) {
                         $.jsNum = res.data.jsNum
                         $.totals = res.data.totals
                         $.drawCondition = res.data.activityVo.drawCondition
-                        if ($.index == 1) {
-                            $.headHelpTimes = $.totals - $.jsNum
-                            console.log(`车头账号需要助力的次数为${$.headHelpTimes}次`)
-                            $.otherHelpTime = $.drawCondition - $.jsNum <= 0 ? $.headHelpTimes : $.drawCondition - $.jsNum
-                            console.log(`每个账号需要助力的次数为${$.otherHelpTime}次即可达到开奖要求！`)
-                            $.friendUuid = $.friendUuids[0]
-                            console.log(`接下来都会助力${$.friendUuid}`)
-                            $.helpTimes = $.headHelpTimes
-                        }
+                        // if ($.index == 1) {
+                        $.headHelpTimes = $.totals - $.jsNum
+                        console.log(`车头账号需要助力的次数为${$.headHelpTimes}次`)
+                        $.otherHelpTime = $.drawCondition - $.jsNum <= 0 ? $.headHelpTimes : $.drawCondition - $.jsNum
+                        console.log(`每个账号需要助力的次数为${$.otherHelpTime}次即可达到开奖要求！`)
+                        $.friendUuid = $.friendUuids[0]
+                        console.log(`接下来都会助力${$.friendUuid}`)
+                        $.helpTimes = $.otherHelpTime
+                        // }
                     } else if (res.errorMessage) {
                         console.log(`${type} ${res.errorMessage || ''}`)
                     } else {
